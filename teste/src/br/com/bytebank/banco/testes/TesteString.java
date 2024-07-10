e3ca5de4110e444edc83bbedb054af91e8f38e4a
sbm-ocampagnoli <otavio.campagnoli@sbmtechnology.com.br>
2024-07-10 10:14:31 -0300

    String e imutabilidade

---
+++ teste/src/br/com/bytebank/banco/testes/TesteString.java
@@ -0,0 +1,13 @@
+package br.com.bytebank.banco.testes;
+
+public class TesteString {
+	public static void main(String[] args) {
+
+		String nome = "AlurA";
+
+		String outra = nome.replace("A", "a");
+
+		System.out.println(outra);
+		System.out.println(nome);
+	}
+}
