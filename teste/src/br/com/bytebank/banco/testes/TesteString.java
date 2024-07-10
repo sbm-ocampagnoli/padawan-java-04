bbb4b96d9ef38c60e830577150a8d77fff745251
sbm-ocampagnoli <otavio.campagnoli@sbmtechnology.com.br>
2024-07-10 10:46:34 -0300

    Metodo da Classe String

--- teste/src/br/com/bytebank/banco/testes/TesteString.java
+++ teste/src/br/com/bytebank/banco/testes/TesteString.java
@@ -9,5 +9,35 @@ public class TesteString {
 
 		System.out.println(outra);
 		System.out.println(nome);
+
+		String nomeTeste = "Otavio          ";
+
+		String teste = nomeTeste.trim();
+
+		System.out.println(nomeTeste);
+		System.out.println(teste);
+		
+		String nomeTeste02 = "Otavio";
+
+		String teste02 = nomeTeste.substring(2);
+
+		System.out.println("\n" + nomeTeste02);
+		System.out.println(teste02);
+		
+		String nomeTeste03 = "Otavio";
+
+		int posicao = nomeTeste03.length() - 1;
+
+		System.out.println("\n" + nomeTeste03);
+		System.out.println(posicao);
+		
+		String vazio = "Alura";
+		
+		System.out.println(vazio.contains("Alu"));
+		
+		
+		
+		
 	}
+
 }
